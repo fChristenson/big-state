f5b12c49727f5d1db29c7358baf792a122bab31b
fChristenson <fredrik.christenson@gmail.com>
2020-03-22 19:37:53 +0100

    wp

--- src/public/ts/overview/duck.tsx
+++ src/public/ts/overview/duck.tsx
@@ -5,8 +5,7 @@ export interface IState {
 }
 
 export enum ActionType {
-  SET_DATA = "SET_DATA",
-  ADD_DATA = "ADD_DATA"
+  SET_DATA = "SET_DATA"
 }
 
 interface IAction {
@@ -22,9 +21,6 @@ export const reducer = (state: IState = initState, action: IAction) => {
   switch (action.type) {
     case ActionType.SET_DATA:
       return Object.assign({}, state, { data: action.payload });
-
-    case ActionType.ADD_DATA:
-      return Object.assign({}, state, { data: state.data.concat(action.payload) });
 
     default:
       return state;
